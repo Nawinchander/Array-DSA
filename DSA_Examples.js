e8554be1151aa06ba1edf8bea438c54bde521b15
Nawin chander <nawinnapzy289@gmail.com>
2026-03-18 07:59:28 +0530

    add the code

--- DSA_Examples.js
+++ DSA_Examples.js
@@ -13,5 +13,31 @@ function findMax(arr) {
 console.log(findMax([3, 7, 2, 9])); // 9
 
 
+/// Reverse Array
+
+function reverseArray(arr) {
+  let left = 0, right = arr.length - 1;
+
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+
+  return arr;
+}
+
+console.log(reverseArray([1, 2, 3, 4]));
+
+//// Remove Duplicates
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
+
+
+
 
 
