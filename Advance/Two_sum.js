f6b1aabb31af2647cf0bcf348886455eac95ef03
Nawin chander <nawinnapzy289@gmail.com>
2026-03-18 08:02:22 +0530

    add the code and file

--- Advance/Two_sum.js
+++ Advance/Two_sum.js
@@ -1,3 +1,22 @@
 /// Optimal Approach (HashMap) — O(n)
 
 
+function twoSum(arr, target) {
+  let map = new Map(); // value -> index
+
+  for (let i = 0; i < arr.length; i++) {
+    let complement = target - arr[i];
+
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+
+    map.set(arr[i], i);
+  }
+
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
+
+
