1d79f4825501702fa87a268d047a6c90a02c2cf4
Nawin chander <nawinnapzy289@gmail.com>
2026-03-18 08:03:30 +0530

    add the code

---
+++ Advance/Container With Most Water.js
@@ -0,0 +1,31 @@
+//// Container With Most Water (Two Pointer)
+
+
+function maxArea(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxWater = 0;
+
+  while (left < right) {
+    let width = right - left;
+    let h = Math.min(height[left], height[right]);
+    let area = width * h;
+
+    maxWater = Math.max(maxWater, area);
+
+    // move smaller height pointer
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxWater;
+}
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
+
+
+
+
