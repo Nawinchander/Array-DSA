dba8f78e30d25ac1831c743b9e95571a5bdff43e
Nawin chander <nawinnapzy289@gmail.com>
2026-03-19 08:46:39 +0530

    add the code

--- Types/script.js
+++ Types/script.js
@@ -74,5 +74,67 @@ arr.pop();
 console.log(arr); // [10, 20]
 
 
+//// Sparse Array
+
+let arr = [];
+arr[0] = 10;
+arr[5] = 50;
+
+console.log(arr); 
+// [10, empty × 4, 50]
+
+console.log(arr.length); // 6
+
+//// Jagged Array (Irregular Array)
+
+let jagged = [
+  [1, 2],
+  [3, 4, 5],
+  [6]
+];
+
+console.log(jagged[1][2]); // 5
+
+
+//// Circular Array (Conceptual)
+
+class CircularArray {
+  constructor(size) {
+    this.arr = new Array(size);
+    this.size = size;
+  }
+
+  get(index) {
+    return this.arr[index % this.size];
+  }
+
+  set(index, value) {
+    this.arr[index % this.size] = value;
+  }
+}
+
+let c = new CircularArray(3);
+c.set(0, 10);
+c.set(3, 20); // overwrites index 0
+
+console.log(c.get(0)); // 20
+
+
+//// Prefix Sum Array (Important for DSA)
+
+let arr = [1, 2, 3, 4, 5];
+let prefix = [arr[0]];
+
+for (let i = 1; i < arr.length; i++) {
+  prefix[i] = prefix[i - 1] + arr[i];
+}
+
+console.log(prefix); // [1, 3, 6, 10, 15]
+
+// Range sum (1 to 3)
+let sum = prefix[3] - prefix[0];
+console.log(sum); // 9
+
+
 
 
