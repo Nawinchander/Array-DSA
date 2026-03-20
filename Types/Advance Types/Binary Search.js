fc86c9e3dacba27d34b7a2bdfb67d1c98ca12996
Nawin chander <nawinnapzy289@gmail.com>
2026-03-20 09:23:26 +0530

    add code

--- Types/Advance Types/Binary Search.js
+++ Types/Advance Types/Binary Search.js
@@ -1,5 +1,26 @@
 // Binary Search — Find Minimum in Rotated Sorted Array
 
+function findMin(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return nums[left];
+}
+
+// Example
+console.log(findMin([4,5,6,7,0,1,2])); // 0
+
+
 
 
 
