8611448e45281201f2ef294a9a82e13e6cdff958
Nawin chander <nawinnapzy289@gmail.com>
2026-03-20 09:24:59 +0530

    add the code

--- Types/Advance Types/Monotonic Stack.js
+++ Types/Advance Types/Monotonic Stack.js
@@ -1,1 +1,26 @@
 /// Monotonic Stack — Next Greater Element
+
+
+
+function nextGreaterElement(nums) {
+  let result = new Array(nums.length).fill(-1);
+  let stack = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    while (stack.length && nums[i] > nums[stack[stack.length - 1]]) {
+      let index = stack.pop();
+      result[index] = nums[i];
+    }
+    stack.push(i);
+  }
+
+  return result;
+}
+
+// Example
+console.log(nextGreaterElement([2,1,2,4,3])); 
+// [4,2,4,-1,-1]
+
+
+
+
